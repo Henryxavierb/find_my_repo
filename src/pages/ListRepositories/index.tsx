1689b00efd493eb01f1ccf714e64fb7d0a2d5724
Henrique Barros <henriquexavierb@gmail.com>
2020-07-10 02:28:22 -0300

    Create a list of searched repositorys

---
+++ src/pages/ListRepositories/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import "./index.css";
+import { useHistory } from "react-router-dom";
+import Header from "../../components/HeaderBar";
+import Repository from "../../components/Repository";
+
+interface RepositoryModel {
+  location?: any;
+}
+
+interface OptionsModel {
+  name: string;
+  language: string;
+  html_url: string;
+  description: string;
+  stargazers_count: string;
+}
+
+const ListRepositories: React.FC<RepositoryModel> = (props) => {
+  const { location } = props;
+  const { state } = location;
+  const history = useHistory();
+
+  if (!state) {
+    history.push("/");
+  }
+
+  const noRepositoryFound = state.options.length === 0;
+
+  return (
+    <div className="listRoot">
+      <Header />
+
+      <div className="historyNavigation">
+        <div className="link" onClick={() => history.push("/")}>
+          Search
+        </div>
+
+        <p className="ctrlBar"> / </p>
+
+        <div className="linkRepo" style={{ fontWeight: "bold" }}>
+          Repositories
+        </div>
+      </div>
+
+      <div style={{ marginTop: "50px" }}>
+        <h1 className="title">Resultados da pesquisa "{state.search}"</h1>
+
+        {noRepositoryFound ? (
+          <h2 className="noRepositoryFound">Nenhum repositório encontrado!</h2>
+        ) : (
+          <div className="listContainer">
+            {state.options?.map((tuple: OptionsModel) => {
+              return (
+                <Repository
+                  name={tuple.name}
+                  key={tuple.html_url}
+                  language={tuple.language}
+                  description={tuple.description}
+                  starsCount={tuple.stargazers_count}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ListRepositories;
