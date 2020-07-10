ecc5d33f9f9bbb49ff4cb5caaec05a7df76e8249
Henrique Barros <henriquexavierb@gmail.com>
2020-07-10 18:34:01 -0300

    Create a route to repository page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Repository from "./pages/Repository";
 import ListRepositories from "./pages/ListRepositories";
 
 const Routes: React.FC = () => {
@@ -13,6 +14,10 @@ const Routes: React.FC = () => {
     {
       path: "/repositories",
       component: ListRepositories,
+    },
+    {
+      path: "/repository",
+      component: Repository,
     },
   ];
 
