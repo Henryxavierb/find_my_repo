50a4778a1f48be2aa15c3aea85366411df69b499
Henrique Barros <henriquexavierb@gmail.com>
2020-07-10 18:47:56 -0300

    Add preface repository to repository page

---
+++ src/pages/Repository/PrefaceRepository.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./index.css";
+import starImage from "../../assets/star.svg";
+
+interface PrefaceModel {
+  url: string;
+  owner: string;
+  stars?: number;
+  UserUrl?: string;
+  projectName: string;
+  description?: string;
+}
+
+const PrefaceRepository: React.FC<PrefaceModel> = (props) => {
+  const { description, UserUrl, owner, projectName, url, stars } = props;
+
+  return (
+    <div className="prefaceRoot">
+      <div className="navigation">
+        <a href={UserUrl}>{owner}</a>
+        <div> / </div>
+        <a href={url}>{projectName}</a>
+      </div>
+
+      <div className="prefaceDescription">
+        {description || "No description, website, or topics provided."}
+      </div>
+
+      <div className="stars">
+        <img src={starImage} alt="stars" />
+        <div>{stars}</div>
+      </div>
+    </div>
+  );
+};
+
+export default PrefaceRepository;
